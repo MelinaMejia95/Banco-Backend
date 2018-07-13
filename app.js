aa6eca607e7b3df3cc9f0a44efa298b55894f647
Melina Mejia Bedoya <melinamejia95@gmail.com>
2018-07-13 16:23:17 -0500

    Sql request configured

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
-app.use('/banco', function (req, res) {
+app.all("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 
-    //console.dir(req)
-        
-    req.header("Access-Control-Allow-Origin", "*");    
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.post('/banco', function (req, res) {
 
     var sql = require("mssql");
 
@@ -29,20 +31,31 @@ app.use('/banco', function (req, res) {
     };
 
     sql.connect(config).then(function () {
-        
-        //Inline query without parameter 
+
         new sql.Request()
-        .query("INSERT INTO Cliente (Id, Nombre, Apellido) VALUES (" + req.body.id + ",'" + req.body.nombre + "','" + req.body.apellido + "')").then(function (recordset) {
-            console.dir(res)
-            //console.dir(recordset.recordsets);
-            res.send('Registro creado')
+        .query("select id from Cliente where id=" + req.body.id).then(function (result){
+            if(result.rowsAffected != 0){
+                res.send('Duplicado')
+                sql.close()
+            } else {
+                    new sql.Request()
+                    .query("INSERT INTO Cliente (Id, Nombre, Apellido, Fecha) VALUES (" + req.body.id + ",'" + req.body.nombre + "','" + req.body.apellido + "','" + req.body.fecha + "')").then(function (recordset) {
+                        res.send('Registro creado')
+                        sql.close()
+                    }).catch(function (err) {
+                        console.log(err);
+                        sql.close()
+                    });
+            }
         }).catch(function (err) {
             console.log(err);
+            sql.close()
         });
-      
     })
 });
 
-var server = app.listen(5000, function () {
+
+
+var server = app.listen(5001, function () {
     console.log('Server is running..');
 });
